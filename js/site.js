079213be8eb93d1d185bc9837002330039cfa2d0
Antonio Jesus Ríos Cabrera <antonio.rios.cabrera@avanade.com>
2018-06-19 18:51:17 +0200

    Bootstrap COurse: Module 6

--- js/site.js
+++ js/site.js
@@ -14,4 +14,33 @@
         $otherButton.text(other);
     });
 
+    var $sentDialog = $("#sentDialog");
+
+    $("#contactForm").on("submit", function(){
+        $sentDialog.modal('show');
+        return false;
+    });
+
+    var $sentAlert = $("#sentAlert");
+
+    $sentDialog.on("hidden.bs.modal", function(){
+        //alert("close");
+        $sentAlert.show();
+    });
+
+    $sentAlert.on("close.bs.alert", function(){
+        $sentAlert.hide();
+        return false;
+    });
+
+    $("#contactForm input[type=submit]").tooltip({
+        placement: "left",
+        delay: {
+            show: 500,
+            hide: 0
+        }
+    });
+
+    $("#theCarousel").carousel();
+
 })();
